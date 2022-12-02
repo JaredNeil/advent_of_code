9f6b2f536cdf0a64e9f2109f6222008fc9eca9bb
Jared Neil <jaredneil@lucidchart.com>
2022-12-02 09:07:13 -0700

    2022 day 2 "optimisations"

--- 2022/02/1.py
+++ 2022/02/1.py
@@ -3,29 +3,27 @@ from parse import load_lines, s
 
 (lines,) = load_lines(repeat(s))
 
+scores = {
+    "A": {
+        "X": 1 + 3,
+        "Y": 2 + 6,
+        "Z": 3 + 0,
+    },
+    "B": {
+        "X": 1 + 0,
+        "Y": 2 + 3,
+        "Z": 3 + 6,
+    },
+    "C": {
+        "X": 1 + 6,
+        "Y": 2 + 0,
+        "Z": 3 + 3,
+    },
+}
+
 score = 0
 for l in lines:
     them, me = l.split()
-    if them == "A":
-        if me == "X":
-            score += 1 + 3
-        elif me == "Y":
-            score += 2 + 6
-        elif me == "Z":
-            score += 3 + 0
-    elif them == "B":
-        if me == "X":
-            score += 1 + 0
-        elif me == "Y":
-            score += 2 + 3
-        elif me == "Z":
-            score += 3 + 6
-    elif them == "C":
-        if me == "X":
-            score += 1 + 6
-        elif me == "Y":
-            score += 2 + 0
-        elif me == "Z":
-            score += 3 + 3
+    score += scores[them][me]
 
 print(score)
